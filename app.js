5567e866757d1ef4d6f14863a71f94942fbc1333
anands11n <anandsingh2926628@gmail.com>
2026-08-28 21:36:33 +0530

    introduced several error handling features to handle some comman errors

--- app.js
+++ app.js
@@ -1,99 +1,91 @@
-const express= require("express");
+const express = require("express");
 const app = express();
-const mongoose= require("mongoose");
-const Listing = require("./models/listing.js")
+const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
 const path = require("path");
-const methodOverride= require("method-override");
-const ejsMate= require("ejs-mate");
-
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wandora";
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine("ejs", ejsMate)
+app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 main()
-    .then(()=>{
-        console.log("Connected to DB");
-    })
-    .catch((err)=>{
-        console.log(err);
-    });
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-async function main(){
-    await mongoose.connect(MONGO_URL);
+async function main() {
+  await mongoose.connect(MONGO_URL);
 }
 
-app.get("/", (req, res)=>{
-    res.send("Working");
+app.get("/", (req, res) => {
+  res.send("Working");
 });
-
-app.listen(8080, ()=>{
-    console.log("Listening on 8080");
-});
-
 
 //index route
-app.get("/listings", async (req, res)=>{
-    const allListings= await Listing.find({});
-    res.render("listings/index.ejs", {allListings});
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 });
-
-
 
 //new route
-app.get("/listings/new", (req, res)=>{
-    res.render("listings/new.ejs")
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
 });
 
-
 //show route
-app.get("/listings/:id", async (req, res)=>{
-    let {id}= req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing})
-
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
 });
 
 //create route
-app.post("/listings", async (req, res)=>{
+app.post(
+  "/listings",
+  wrapAsync(async (req, res, next) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-
-});
+  }),
+);
 
 //edit route
-app.get("/listings/:id/edit", async (req, res)=>{
-    let {id}= req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing});
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
 });
 
 //update route
-app.put("/listings/:id", async (req, res)=>{
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
-    res.redirect(`/listings/${id}`);
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
 });
 
-
 //delete route
-app.delete("/listings/:id", async (req, res)=>{
-    let {id} = req.params;
-    let deletedListing= await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
 });
 
 // app.get("/testListing", async (req, res)=>{
 //     let sampleListing =new Listing({
-//         title: "My New Villa", 
-//         description: "By the beach", 
-//         price: 1200, 
+//         title: "My New Villa",
+//         description: "By the beach",
+//         price: 1200,
 //         location: "Calangute, Goa",
 //         country: "India"
 //     });
@@ -101,3 +93,11 @@ app.delete("/listings/:id", async (req, res)=>{
 //     console.log("Sample was saved");
 //     res.send("Successful saving");
 // });
+
+app.use((err, req, res, next) => {
+  res.send("Something went wrong!");
+});
+
+app.listen(8080, () => {
+  console.log("Listening on 8080");
+});
